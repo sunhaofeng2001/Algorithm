02e0cd4f20aca2298d59e25452bbec1b8c7a3d53
sun haofeng <sunhaofeng2001@gmail.com>
2022-07-28 22:21:19 +0800

    7_28

---
+++ code/ch2/insertion_sort_down.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+template<class T>
+
+int length(T& arr)
+{
+    return sizeof(arr) / sizeof(arr[0]);
+}
+
+void insertion_sort_up(int *A,int len){
+    int num;
+    int insert_num;
+    if(len>1){
+        for(int i=0; i<len-1; i++){
+            if(A[i]<A[i+1]){
+                num = A[i+1];
+                for(int x=i;x>=0;x--){
+                    if(num>A[x]){
+                        insert_num = x;
+                    }
+                }
+                for(int j=i;j>=insert_num;j--){
+                    A[j+1] = A[j];
+                }
+                A[insert_num] = num;
+            }
+        }
+    }
+}
+
+int main(){
+    int A[] = {-1,4};
+    insertion_sort_up(A, length(A));
+    for(int i=0; i<length(A);i++){
+        cout<<A[i]<<" ";
+    }
+}
