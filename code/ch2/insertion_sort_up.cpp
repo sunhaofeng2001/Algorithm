76c76dfbf5a98d0f8fb9eec6d6a360f8f87e91df
sun haofeng <sunhaofeng2001@gmail.com>
2022-07-29 12:47:59 +0800

    'insertion_sort_up_update'

--- code/ch2/insertion_sort_up.cpp
+++ code/ch2/insertion_sort_up.cpp
@@ -7,7 +7,7 @@ int length(T& arr)
     return sizeof(arr) / sizeof(arr[0]);
 }
 
-void insertion_sort_up(int *A,int len){
+void insertion_sort_up1(int *A,int len){
     int num;
     int insert_num;
     if(len>1){
@@ -28,8 +28,32 @@ void insertion_sort_up(int *A,int len){
     }
 }
 
+
+void insertion_sort_up(int *A,int len){
+    int num;
+    int insert_num;
+    int i;
+    if(len>1){
+        for(int j=1; j<len; j++){
+            num = A[j];
+            i = j-1;
+            while (i>=0 & A[i]>num)
+            {
+                A[i+1] = A[i];
+                i = i-1;
+            }
+            A[i+1]=num;
+            for(int x=0; x<len;x++){
+                cout<<A[x]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
+
+
 int main(){
-    int A[] = {-1,9,8,7,6,5,4,3,2,-1,0};
+    int A[] = {9,8,7,6,5,4,3,2,1,0};
     insertion_sort_up(A, length(A));
     for(int i=0; i<length(A);i++){
         cout<<A[i]<<" ";
